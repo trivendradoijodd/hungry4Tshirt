44d8b8f1850ff6b1225bbd5ef1a127c47c19aa91
trivendradoijodd <50183279+trivendradoijodd@users.noreply.github.com>
2019-10-30 15:44:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
 
 class App extends Component{
+state = {
+  var: "Hello World"
+}
 render(){
-return(<div>Hello World</div>)
+return(<div>{this.state.var}</div>)
 }
 }
 
